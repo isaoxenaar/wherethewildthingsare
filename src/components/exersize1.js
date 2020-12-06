bbcae6363115a80c7cb9f0c2c776a41f9f489473
Isa Oxenaar <isamoxenaar@gmail.com>
2020-12-06 08:08:22 +0100

    Set up of program

---
+++ src/components/exersize1.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Exe1 extends Component {
+  render() {
+    return <div>exi 1. NDJSON</div>;
+  }
+}
+
+export default Exe1;
---
+++ src/components/exersize2.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Exe2 extends Component {
+  render() {
+    return <div>exersize 2. Datagraph Permafrost.</div>;
+  }
+}
+
+export default Exe2;
