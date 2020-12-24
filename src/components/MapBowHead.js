3d8253977f3a0f81e726e6845ace419a5df7cc60
Isa Oxenaar <isamoxenaar@gmail.com>
2020-12-24 12:36:38 +0100

    First whalepath visualized

--- src/components/MapBowHead.js
+++ src/components/MapBowHead.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, GeoJSON } from "react-leaflet";
 import { data } from "../D3_Html_Charts/rawdata";
 import "../App.css";
 
@@ -29,6 +29,7 @@ class MapBowHead extends Component {
       ],
     };
     console.log(geojson);
+    console.log(geojson.features[0].geometry);
 
     return (
       <MapContainer center={[78.90136, 13.4838879]} zoom={12}>
@@ -36,6 +37,14 @@ class MapBowHead extends Component {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <Marker
+          key={geojson.features[0].properties.name}
+          position={[
+            geojson.features[0].geometry.coordinates[0][1],
+            geojson.features[0].geometry.coordinates[0][0],
+          ]}
+        ></Marker>
+        <GeoJSON data={geojson}></GeoJSON>
       </MapContainer>
     );
   }
