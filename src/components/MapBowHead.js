7622766cc422ad9f73f223ea31cd53d733cfa1da
Isa Oxenaar <isamoxenaar@gmail.com>
2020-12-24 18:09:20 +0100

    Whales with their own path and color

--- src/components/MapBowHead.js
+++ src/components/MapBowHead.js
@@ -1,50 +1,90 @@
 import React, { Component } from "react";
-import { MapContainer, Marker, Popup, TileLayer, GeoJSON } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  GeoJSON,
+  Polyline,
+} from "react-leaflet";
 import { data } from "../D3_Html_Charts/rawdata";
 import "../App.css";
 
 class MapBowHead extends Component {
   render() {
-    const whale1 = data.filter((w) => {
-      return w.Ind === 168446;
-    });
-
-    const coordinatesWhale1 = whale1.map((w) => {
-      const coordinate = [w.Lon, w.Lat];
-      return coordinate;
-    });
-
-    var geojson = {
-      type: "FeatureCollection",
-      features: [
-        {
-          type: "Feature",
-          id: "01",
-          properties: { name: "168446" },
-          geometry: {
-            type: "LineString",
-            coordinates: coordinatesWhale1,
-          },
-        },
-      ],
+    const uniqueWhales = (array) => {
+      const double = {};
+      const unique = [];
+      array.forEach((e) => {
+        if (!double[e.Ind]) {
+          double[e.Ind] = true;
+          unique.push(e.Ind);
+        }
+      });
+      return unique;
     };
-    console.log(geojson);
-    console.log(geojson.features[0].geometry);
 
+    const whaleNames = uniqueWhales(data);
+
+    const allFeatures = whaleNames.map((n, i) => {
+      const whale = data.filter((w) => {
+        return w.Ind === n;
+      });
+
+      const coordinatesWhale = whale.map((w) => {
+        const coordinate = [w.Lon, w.Lat];
+        return coordinate;
+      });
+
+      const feature = {
+        type: "Feature",
+        id: n,
+        properties: {
+          name: n,
+          color: `rgb(${0 + i * 15}, ${0 + i * 15}, ${0 + i * 15})`,
+          weight: i,
+        },
+        geometry: {
+          type: "LineString",
+          coordinates: coordinatesWhale,
+        },
+      };
+      return feature;
+    });
+    console.log(allFeatures);
+
+    const geoJSON = {
+      type: "FeatureCollection",
+      features: allFeatures,
+    };
+
+    const position = [
+      geoJSON.features[0].geometry.coordinates[0][1],
+      geoJSON.features[0].geometry.coordinates[0][0],
+    ];
     return (
-      <MapContainer center={[78.90136, 13.4838879]} zoom={12}>
+      <MapContainer center={position} zoom={6}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker
-          key={geojson.features[0].properties.name}
-          position={[
-            geojson.features[0].geometry.coordinates[0][1],
-            geojson.features[0].geometry.coordinates[0][0],
-          ]}
+          key={geoJSON.features[0].properties.name}
+          position={position}
         ></Marker>
-        <GeoJSON data={geojson}></GeoJSON>
+        <Polyline
+          positions={geoJSON.features[0].geometry.coordinates}
+          color={geoJSON.features[0].properties.color}
+        ></Polyline>
+        <GeoJSON
+          data={geoJSON}
+          style={(feature) => {
+            return {
+              color: feature.properties.color,
+              weight: feature.properties.weight,
+            };
+          }}
+        ></GeoJSON>
       </MapContainer>
     );
   }
